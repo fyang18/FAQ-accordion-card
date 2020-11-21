ed7b327616f5750e904a04a436113b9afbeeb0a7
fyang18 <francis.yang@hotmail.fr>
2020-11-21 13:52:31 +0100

    project terminated

---
+++ app.js
@@ -0,0 +1,79 @@
+function afficherQ1() {
+    var answer1 = document.getElementById("answer1");
+    var q1 = document.getElementById("q1");
+    var imgQ1 = document.getElementById("imgQ1");
+    if (answer1.style.display == 'none') {
+        answer1.style.display = 'block';
+        q1.style.fontWeight = "bold";
+        imgQ1.style.transform = 'rotate(180deg)';
+
+    } else {
+        answer1.style.display = 'none';
+        q1.style.fontWeight = "normal";
+        imgQ1.style.transform = 'rotate(0deg)';
+    }
+}
+
+function afficherQ2() {
+    var answer2 = document.getElementById("answer2");
+    var q2 = document.getElementById("q2");
+    var imgQ2 = document.getElementById("imgQ2");
+    if (answer2.style.display == 'none') {
+        answer2.style.display = 'block';
+        q2.style.fontWeight = "bold";
+        imgQ2.style.transform = 'rotate(180deg)';
+
+    } else {
+        answer2.style.display = 'none';
+        q2.style.fontWeight = "normal";
+        imgQ2.style.transform = 'rotate(0deg)';
+    }
+}
+
+function afficherQ3() {
+    var answer3 = document.getElementById("answer3");
+    var q3 = document.getElementById("q3");
+    var imgQ3 = document.getElementById("imgQ3");
+    if (answer3.style.display == 'none') {
+        answer3.style.display = 'block';
+        q3.style.fontWeight = "bold";
+        imgQ3.style.transform = 'rotate(180deg)';
+
+    } else {
+        answer3.style.display = 'none';
+        q3.style.fontWeight = "normal";
+        imgQ3.style.transform = 'rotate(0deg)';
+    }
+}
+
+function afficherQ4() {
+    var answer4 = document.getElementById("answer4");
+    var q4 = document.getElementById("q4");
+    var imgQ4 = document.getElementById("imgQ4");
+    if (answer4.style.display == 'none') {
+        answer4.style.display = 'block';
+        q4.style.fontWeight = "bold";
+        imgQ4.style.transform = 'rotate(180deg)';
+
+    } else {
+        answer4.style.display = 'none';
+        q4.style.fontWeight = "normal";
+        imgQ4.style.transform = 'rotate(0deg)';
+    }
+}
+
+function afficherQ5() {
+    var answer5 = document.getElementById("answer5");
+    var q5 = document.getElementById("q5");
+    var imgQ5 = document.getElementById("imgQ5");
+    if (answer5.style.display == 'none') {
+        answer5.style.display = 'block';
+        q5.style.fontWeight = "bold";
+        imgQ5.style.transform = 'rotate(180deg)';
+
+    } else {
+        answer5.style.display = 'none';
+        q5.style.fontWeight = "normal";
+        imgQ5.style.transform = 'rotate(0deg)';
+    }
+}
